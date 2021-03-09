6457ce535584dec95d038b87715c2487c4e1de22
Austin <alaw@sociusmarketing.com>
2021-03-09 12:02:30 -0500

    edits

--- src/templates/contact.js
+++ src/templates/contact.js
@@ -12,29 +12,28 @@ const IndexPage = props => {
       <Container>
         <Row>
           <Col>
+          <h1>Contact VP & Associates</h1>
             <Form>
-              <Form.Group controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
+              <Form.Group controlId="formBasicName">
+                <Form.Label>Name</Form.Label>
+                <Form.Control type="text" placeholder="Name" />
                 <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
                 </Form.Text>
               </Form.Group>
 
-              <Form.Group controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" placeholder="Email" />
               </Form.Group>
-              <Form.Group controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-              </Form.Group>
+
               <Button variant="primary" type="submit">
                 Submit
               </Button>
+              
             </Form>
           </Col>
           <Col>
-            <h1>{title}</h1>
+            <h2>Company Info</h2>
             <article dangerouslySetInnerHTML={{ __html: content }} />
           </Col>
         </Row>
